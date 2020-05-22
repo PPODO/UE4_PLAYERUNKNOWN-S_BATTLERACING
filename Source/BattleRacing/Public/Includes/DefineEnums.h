b98439d3b4c7e02ae6ce26f5c77dd0b4c165b55f
PPODO <sherlockwhooo@gmail.com>
2020-05-23 01:22:33 +0900

    Create DefineEnums

---
+++ Source/BattleRacing/Public/Includes/DefineEnums.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DefineEnums.generated.h"
+
+UENUM(BlueprintType)
+enum class ETeamType : uint8 {
+	Team_Red,
+	Team_Blue
+};
