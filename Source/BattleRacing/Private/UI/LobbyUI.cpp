e78e20146cb805766936c65b9e24bebadaf69a8e
PPODO <sherlockwhooo@gmail.com>
2020-05-27 03:08:06 +0900

    Update LobbyUI

--- Source/BattleRacing/Private/UI/LobbyUI.cpp
+++ Source/BattleRacing/Private/UI/LobbyUI.cpp
@@ -1,6 +1,15 @@
 #include "UI/LobbyUI.h"
+#include "GameStateBase/InGameStateBase.h"
+#include "Engine/World.h"
 
-void ULobbyUI::AddUser_Implementation(ETeamType teamType, FName UserName) {
+void ULobbyUI::NativeConstruct() {
+	Super::NativeConstruct();
 
+	if (auto gameState = Cast<AInGameStateBase>(GetWorld()->GetGameState())) {
+		gameState->mOnNewPlayerEntered.AddDynamic(this, &ULobbyUI::OnNewPlayerEntered);
+	}
+}
 
+void ULobbyUI::OnNewPlayerEntered(const FLobbyUserInformation& newPlayerInformation) {
+	UE_LOG(LogTemp, Error, L"QWEWEQQEW");
 }
--- Source/BattleRacing/Public/UI/LobbyUI.h
+++ Source/BattleRacing/Public/UI/LobbyUI.h
@@ -2,14 +2,17 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
-#include "Includes/DefineEnums.h"
+#include "Includes/DefineStructure.h"
 #include "LobbyUI.generated.h"
 
 UCLASS()
 class BATTLERACING_API ULobbyUI : public UUserWidget {
 	GENERATED_BODY()
+protected:
+	virtual void NativeConstruct() override final;
+
 public:
-	UFUNCTION(NetMulticast, Reliable)
-		void AddUser(ETeamType teamType, FName UserNameW);
+	UFUNCTION()
+		void OnNewPlayerEntered(const FLobbyUserInformation& newPlayerInformation);
 
 };
