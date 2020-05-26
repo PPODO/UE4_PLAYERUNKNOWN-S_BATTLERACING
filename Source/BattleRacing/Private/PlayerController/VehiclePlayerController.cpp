ddc0a81227a9d50f149bbb40dc3e7e5ed745d500
PPODO <sherlockwhooo@gmail.com>
2020-05-27 03:07:41 +0900

    Update PlayerController

--- Source/BattleRacing/Private/PlayerController/VehiclePlayerController.cpp
+++ Source/BattleRacing/Private/PlayerController/VehiclePlayerController.cpp
@@ -1,8 +1,29 @@
 #include "PlayerController/VehiclePlayerController.h"
+#include "GameStateBase/InGameStateBase.h"
+#include "GameInstance/DefaultGameInstance.h"
+#include "GameFramework/PlayerState.h"
 #include "Kismet/GameplayStatics.h"
+#include "UI/LobbyUI.h"
 #include "Camera/CameraActor.h"
 #include "Engine/Engine.h"
+#include "Engine/World.h"
 
 AVehiclePlayerController::AVehiclePlayerController() {
-	//SetReplicates(true);
+	SetReplicates(true);
 }
+
+void AVehiclePlayerController::BeginPlay() {
+	Super::BeginPlay();
+
+}
+
+void AVehiclePlayerController::Tick(float deltaTime) {
+	Super::Tick(deltaTime);
+	
+}
+
+int32 AVehiclePlayerController::GetPlayerID() {
+	if (auto ps = GetPlayerState<APlayerState>())
+		return ps->PlayerId;
+	return -1;
+};
--- Source/BattleRacing/Public/PlayerController/VehiclePlayerController.h
+++ Source/BattleRacing/Public/PlayerController/VehiclePlayerController.h
@@ -11,9 +11,16 @@ class BATTLERACING_API AVehiclePlayerController : public APlayerController {
 public:
 	AVehiclePlayerController();
 
+protected:
+	virtual void BeginPlay() override final;
+	virtual void Tick(float deltaTime) override final;
+
 public:
 	FORCEINLINE void SetTeamType(const ETeamType& type) { mTeamType = type; }
 	FORCEINLINE ETeamType GetTeamType() { return mTeamType; }
+
+	FORCEINLINE bool IsCurrentPlayer(int32 playerID) { if (GetPlayerID() == playerID) return true; return false; };
+	int32 GetPlayerID();
 
 private:
 	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
